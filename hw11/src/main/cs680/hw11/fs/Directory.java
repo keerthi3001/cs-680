f54135276f416e4b10db63e3b5615b2611e1548c
keerthi3001 <keerthikalakoti3006@gmail.com>
2025-12-11 21:28:55 -0500

    Add files via upload

---
+++ hw11/src/main/cs680/hw11/fs/Directory.java
@@ -0,0 +1,99 @@
+package cs680.hw11.fs;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+/**
+ * Directory node in the file system tree.
+ */
+public class Directory extends FSElement {
+
+    private LinkedList<FSElement> children = new LinkedList<>();
+
+    public Directory(Directory parent, String name, LocalDateTime creationTime) {
+        // directory size itself is 0; total size is sum of children
+        super(parent, name, 0, creationTime);
+        if (parent != null) {
+            parent.appendChild(this);
+        }
+    }
+
+    public void appendChild(FSElement child) {
+        if (child == null) {
+            return;
+        }
+        child.setParent(this);
+        children.add(child);
+        // default order: alphabetical
+        Collections.sort(children, new AlphabeticalComparator());
+    }
+
+    public LinkedList<FSElement> getChildren() {
+        return getChildren(new AlphabeticalComparator());
+    }
+
+    public LinkedList<FSElement> getChildren(Comparator<FSElement> cmp) {
+        LinkedList<FSElement> copy = new LinkedList<>(children);
+        copy.sort(cmp);
+        return copy;
+    }
+
+    public LinkedList<Directory> getSubDirectories() {
+        return getSubDirectories(new AlphabeticalComparator());
+    }
+
+    public LinkedList<Directory> getSubDirectories(Comparator<FSElement> cmp) {
+        return getChildren(cmp).stream()
+                .filter(FSElement::isDirectory)
+                .map(e -> (Directory) e)
+                .collect(Collectors.toCollection(LinkedList::new));
+    }
+
+    public LinkedList<File> getFiles() {
+        return getFiles(new AlphabeticalComparator());
+    }
+
+    public LinkedList<File> getFiles(Comparator<FSElement> cmp) {
+        return getChildren(cmp).stream()
+                .filter(FSElement::isFile)
+                .map(e -> (File) e)
+                .collect(Collectors.toCollection(LinkedList::new));
+    }
+
+    public LinkedList<Link> getLinks() {
+        return getLinks(new AlphabeticalComparator());
+    }
+
+    public LinkedList<Link> getLinks(Comparator<FSElement> cmp) {
+        return getChildren(cmp).stream()
+                .filter(FSElement::isLink)
+                .map(e -> (Link) e)
+                .collect(Collectors.toCollection(LinkedList::new));
+    }
+
+    @Override
+    public boolean isDirectory() {
+        return true;
+    }
+
+    @Override
+    public boolean isFile() {
+        return false;
+    }
+
+    @Override
+    public boolean isLink() {
+        return false;
+    }
+
+    @Override
+    public void accept(FSVisitor visitor) {
+        visitor.visit(this);
+        for (FSElement child : children) {
+            child.accept(visitor);
+        }
+    }
+}
